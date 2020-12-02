85f75b738a6785f7e96ec946c55893bac43f25b4
ahmed eid <ahmed.eid.1@hotmail.com>
2020-12-02 18:22:51 +0200

    commit 3

---
+++ log-in/login-process.php
@@ -0,0 +1,28 @@
+<?php
+include 'function/contact DB.php';
+if($_SERVER['REQUEST_METHOD']=='POST'){
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $hashpass = sha1($password);
+
+    $stmt = $con->prepare("SELECT
+                          username,email,Password,UserID
+                           FROM
+                            users
+                           WHERE
+                             email = ?
+                           AND
+                              Password = ?
+                           LIMIT  1");
+    $stmt->execute(array($email , $hashpass));
+    $row = $stmt -> fetch();
+    $count = $stmt->rowCount();
+
+    if($count > 0){
+        $_SESSION['username'] = $row['username'];
+        $_SESSION['ID'] = $row['UserID'];
+        header('Location:dashboard.php');
+        exit();
+
+    }
+}
