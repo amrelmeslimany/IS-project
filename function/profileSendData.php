74634503553a4e634076819903842e6cc1095c2f
ahmed eid <ahmed.eid.1@hotmail.com>
2020-12-05 23:31:30 +0200

    commit 4

--- function/profileSendData.php
+++ function/profileSendData.php
@@ -2,39 +2,22 @@
 ob_start();
 session_start();
 include "contactDB.php";
-if (isset($_SESSION['ID'])) {
-    if (isset($_GET['do'])) {
-        $do = $_GET['do'];
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+    $id = $_SESSION["ID"];
+    $pass = '';
+    if (empty($_POST['newPass'])) {
+        $pass = $_POST['oldPass'];
     } else {
-
+        $pass = sha1($_POST['newPass']);
     }
-    if ($do == 'updete') {//manage page
-        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
-            $id = $_SESSION["ID"];
-            $email = $_POST['email'];
-            $name = $_POST['username'];
-            $img = $_POST['img'];
-            //password trick
-            $pass = '';
-            if (empty($_POST['newPass'])) {
-                $pass = $_POST['oldPass'];
-            } else {
-                $pass = sha1($_POST['newPass']);
-            }
+    $stmt = $con->prepare("UPDATE student SET password= ? WHERE id = ?");
+    $stmt->execute(array($pass, $id));
 
-            $stmt = $con->prepare("UPDATE student SET name = ?,password= ? , email = ? , img = ?  WHERE id = ?");
-            $stmt->execute(array($name, $pass, $email,$img, $id));
-
-        } else {
-
-        }
-
-    } elseif ($do == 'add') {//add members pag
-
-    } else {
-
-    }
 }
+
+
+
 ob_end_flush();
 
