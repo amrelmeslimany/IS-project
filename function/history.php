a1b478b55488da7447f97c5bf52a82d8b134f91d
ahmed eid <ahmed.eid.1@hotmail.com>
2020-12-08 22:22:07 +0200

    commit

--- function/history.php
+++ function/history.php
@@ -1,22 +1,4 @@
 <?php
-function history($id){
-    include "contactDB.php";
-    $stmt = $con->prepare("SELECT
-                                register.`id_course` AS 'code',course.name,course.Credithours AS'hours',
-                                department.name,register.term,register.year
-                            FROM
-                                (register
-                            INNER JOIN course 
-                            ON register.id_course=course.code)
-                            INNER JOIN department
-                            ON course.department=department.code
-                            WHERE 
-                                register.id_student=1
-                                
-                           ");
-    $stmt->execute(array($id));
-    return $row = $stmt -> fetchall();
-}
 function since($id){
     include "contactDB.php";
     $stmt = $con->prepare("SELECT
